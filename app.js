4bd226871fbc41ff479b5dbc0475aabc3d231b01
mvizm2015 <mariaviz2015@gmail.com>
2018-05-13 01:32:01 -0400

    Passport config - require token in header

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const config = require('./config/database');
 // Connect to Database
 mongoose.connect(config.database);
 
-// On Connection
+// On Connectionexit
 mongoose.connection.on('connected', () => {
     console.log('Connected to database '+config.database);
 });
@@ -35,6 +35,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Body Parser Middleware
 app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
 
 app.use('/users', users);
 
