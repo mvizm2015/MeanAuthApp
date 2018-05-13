ad76787c16af13938da91031d1f510962d5e34a6
mvizm2015 <mariaviz2015@gmail.com>
2018-05-13 01:00:18 -0400

    POST user

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const config = require('../models/database');
+const config = require('../config/database');
 
 // User Schema
 const UserSchema = mongoose.Schema({
