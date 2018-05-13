063260f0316b1d8150a54429d71d2151ddd64c63
mvizm2015 <mariaviz2015@gmail.com>
2018-05-12 23:02:35 -0400

    initialize and setup server

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors'); //middleware
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Port Number
+const port = 3000;
+
+// CORS Middleware
+app.use(cors());
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+// Index Route
+app.get('/', (req, res) => {
+    res.send('Invalid endpoint');
+});
+
+// Start Server
+app.listen(port, () => {
+    console.log('Server started on port '+port);
+});
